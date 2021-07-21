9fd936e103d1ab067b5e8c48675bbc5de4205ce7
Fayette Klaassen <klaassen.fayette@gmail.com>
2021-07-21 10:03:04 -0400

    update vacEff and maxCov
    update vaccine effectiveness and max coverage with references

--- R/run.R
+++ R/run.R
@@ -8,7 +8,7 @@ run <- function() {
   maxCoverage <- c(
     # age0-12,12-15,16-17kids
     #https://www.abcactionnews.com/news/coronavirus/kff-research-3-in-10-parents-of-kids-12-15-will-get-their-child-vaccinated-for-covid-19-right-away
-    .5, .75,.75, 
+    .77, .77,.76, 
 
     # age 18-24,25-39,40-49adults
     # https://www.kff.org/coronavirus-covid-19/dashboard/kff-covid-19-vaccine-monitor-dashboard/
@@ -18,14 +18,28 @@ run <- function() {
     .87, .92, .92, 
 
     #composites, for the age groups 0-18;18-65 and 65-99)
-    mean(c(.5,.75,.75)), mean(c(.87,.85,.82,.82)), .92
+    mean(c(.77,.77,.76)), mean(c(.85,.82,.82,.87)), mean(c(.92,.92))
   ) 
 
 
   nda  <- 14
   endDate <- as.Date("2022-12-31")
-  vacEff <- .8
-  sevEff <- .598
+  
+  ## Refernces for Vaccine effectiveness.
+  ## Overview of Vaccine effectiveness research
+  ## https://www.cdc.gov/coronavirus/2019-ncov/science/science-briefs/fully-vaccinated-people.html#ref15
+  
+  ## .875 is the average of .89 (ref 1 below) and .86 (ref 2 below) reported 
+  ## for reduced Covid infection in general US population
+  ## .68 is derived from the vaccine effectiveness for hospitalization 
+  ## in the US general population of .96 (ref 3 below), which is used as
+  ## a substitute for reduced deaths
+  ## (1-.875)*(1-.68) = (1-.96)
+  ### 1 ### Pawlowski C LP, Puranik A, et. al. FDA-authorized COVID-19 vaccines are effective per real-world evidence synthesized across a multi-state health system. medRxiv. 2021;https://www.medrxiv.org/content/10.1101/2021.02.15.21251623v1.full.pdfpdf iconexternal icon.
+  ### 2 ### Andrejko K. PJ, Myers JF., et al. Early evidence of COVID-19 vaccine effectiveness within the general population of California. MedRxiv. 2021;https://www.medrxiv.org/content/10.1101/2021.04.08.21255135v1external icon.
+  ### 3 ### Vahidy FS. PL, Tano ME., et al. Real World Effectiveness of COVID-19 mRNA Vaccines against Hospitalizations and Deaths in the United States. medRxiv. 2021;https://www.medrxiv.org/content/10.1101/2021.04.21.21255873v1external icon.
+  vacEff <- .875 ##
+  sevEff <- .68
 
   provisionalDeathsSystemPath <-
     "Provisional_COVID-19_Death_Counts_by_Sex__Age__and_State_01-10-2021.csv"
